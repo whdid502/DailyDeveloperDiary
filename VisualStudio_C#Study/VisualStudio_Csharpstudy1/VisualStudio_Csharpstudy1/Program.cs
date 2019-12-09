0347ff17a6e9a75115917718e5c83d24e08aa470
whdid502 <whdid1234@naver.com>
2019-12-09 15:21:52 +0900

    191209_chaper2

---
+++ VisualStudio_C#Study/VisualStudio_Csharpstudy1/VisualStudio_Csharpstudy1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;   
+
+namespace VisualStudio_Csharpstudy1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start_program_at_vscs");
+        }
+    }
+}
---
+++ VisualStudio_C#Study/VisualStudio_Csharpstudy1/study2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace study2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("study2");
+        }
+    }
+}
